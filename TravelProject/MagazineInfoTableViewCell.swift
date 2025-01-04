1c096cbe93470a21aa38fd4fed4db4f684ae096e
been <leebin0603@gmail.com>
2025-01-04 14:34:24 +0900

    TRAVEL 탭 구현

---
+++ TravelProject/MagazineInfoTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MagazineInfoTableViewCell.swift
+//  TravelProject
+//
+//  Created by 이빈 on 1/3/25.
+//
+
+import UIKit
+
+class MagazineInfoTableViewCell: UITableViewCell {
+    
+    @IBOutlet var mainImageView: UIImageView!
+    @IBOutlet var mainTitleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+}
