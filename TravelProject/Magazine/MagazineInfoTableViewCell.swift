70457099111fe292eb53f7a6917b2706c02c07c1
been <leebin0603@naver.com>
2025-01-06 19:47:05 +0900

    Magazine 탭: 셀 디자인 코드 리팩토링

--- TravelProject/Magazine/MagazineInfoTableViewCell.swift
+++ TravelProject/Magazine/MagazineInfoTableViewCell.swift
@@ -9,9 +9,79 @@ import UIKit
 
 class MagazineInfoTableViewCell: UITableViewCell {
     
+    static let identifier = "MagazineInfoTableViewCell"
+    
     @IBOutlet var mainImageView: UIImageView!
     @IBOutlet var mainTitleLabel: UILabel!
     @IBOutlet var subTitleLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
+    
+//    var dateFormat: String {
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "yyMMdd"
+//        dateFormatter.timeZone = TimeZone(identifier: "UTC")
+//        
+//        if let date = dateFormatter.date(from: dateLabel.text ?? "") {
+//            dateFormatter.dateFormat = "yy년 M월 d일"
+//            let stringDate = dateFormatter.string(from: date)
+//            return stringDate
+//        } else {
+//            return dateLabel.text ?? ""
+//        }
+//    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configure()
+    }
+    
+    func configure() {
+        // 메인 이미지
+        mainImageView.contentMode = .scaleAspectFill
+        mainImageView.layer.cornerRadius = 10
+        
+        // 메인 타이틀
+        mainTitleLabel.font = .systemFont(ofSize: 24, weight: .heavy)
+        mainTitleLabel.numberOfLines = 0
+        
+        // 서브 타이틀
+        subTitleLabel.font = .systemFont(ofSize: 14)
+        subTitleLabel.textColor = .darkGray
+        subTitleLabel.numberOfLines = 0
+        
+        // 날짜 레이블
+        dateLabel.font = .systemFont(ofSize: 13)
+        dateLabel.textColor = .lightGray
+    }
+    
+    func configureData(row: Magazine) {
+        // 이미지 등록
+        if let url = URL(string: row.photo_image) {
+            mainImageView.kf.setImage(with: url)
+        } else {
+            mainImageView.image = UIImage(systemName: "arrow.down.app.dashed.trianglebadge.exclamationmark")
+        }
+        
+        // 메인타이틀 설정
+        mainTitleLabel.text = row.title
+        
+        // 서브타이틀 설정
+        subTitleLabel.text = row.subtitle
+        
+        // date 설정
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyMMdd"
+        dateFormatter.timeZone = TimeZone(identifier: "UTC")
+        if let date = dateFormatter.date(from: row.date) {
+            dateFormatter.dateFormat = "yy년 M월 d일"
+            let stringDate = dateFormatter.string(from: date)
+            dateLabel.text = stringDate
+            print(dateFormatter.string(from: date))
+        } else {
+            dateLabel.text = row.date
+            print("오류")
+        }
+        
+    }
     
 }
--- TravelProject/Magazine/MagazineInfoTableViewController.swift
+++ TravelProject/Magazine/MagazineInfoTableViewController.swift
@@ -16,55 +16,15 @@ class MagazineInfoTableViewController: UITableViewController {
         super.viewDidLoad()
     }
     
-    // 1. 셀의 개수
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return magazine.count
     }
     
-    // 2. 셀의 디자인
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MagazineInfoTableViewCell", for: indexPath) as! MagazineInfoTableViewCell
-
-        let row = magazine[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: MagazineInfoTableViewCell.identifier, for: indexPath) as! MagazineInfoTableViewCell
         
-        // 이미지 등록
-        if let url = URL(string: row.photo_image) {
-            cell.mainImageView.kf.setImage(with: url)
-        } else {
-            cell.mainImageView.image = UIImage(systemName: "arrow.down.app.dashed.trianglebadge.exclamationmark")
-        }
-        
-        cell.mainImageView.contentMode = .scaleAspectFill
-        cell.mainImageView.layer.cornerRadius = 10
-        
-        // 메인타이틀 설정
-        cell.mainTitleLabel.text = row.title
-        cell.mainTitleLabel.font = .systemFont(ofSize: 24, weight: .heavy)
-        cell.mainTitleLabel.numberOfLines = 0
-        
-        // 서브타이틀 설정
-        cell.subTitleLabel.text = row.subtitle
-        cell.subTitleLabel.font = .systemFont(ofSize: 14)
-        cell.subTitleLabel.textColor = .darkGray
-        cell.subTitleLabel.numberOfLines = 0
-        
-        // 데이트 설정
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyMMdd"
-        dateFormatter.timeZone = TimeZone(identifier: "UTC")
-        if let date = dateFormatter.date(from: row.date) {
-            dateFormatter.dateFormat = "yy년 M월 d일"
-            let stringDate = dateFormatter.string(from: date)
-            cell.dateLabel.text = stringDate
-            print(dateFormatter.string(from: date))
-        } else {
-            cell.dateLabel.text = row.date
-            print("오류")
-        }
-        
-        cell.dateLabel.font = .systemFont(ofSize: 13)
-        cell.dateLabel.textColor = .lightGray
+        cell.configureData(row: magazine[indexPath.row])
         
         return cell
     }
